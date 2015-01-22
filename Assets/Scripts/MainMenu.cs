52418b96fc933e3703c27d56767c4cb7f70d54d6
Calem Bendell <calem.j.bendell@gmail.com>
2015-01-22 18:15:06 -0500

    started looking at building main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+  public GUISkin skin;
+
+  Vector2 scrollPos = new Vector2();
+  string description = "";
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void onGUI()
+  {
+    GUI.skin = this.skin;
+    GUILayout.Space(10);
+
+    GUILayout.BeginHorizontal();
+    GUILayout.Space(10);
+    scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Width(400));
+
+  }
+}
